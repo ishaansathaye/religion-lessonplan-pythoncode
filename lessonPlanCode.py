adba9c3dc13b9a0caf0b85bea0aae7ea2360a028
Ishaan Sathaye <Ishaan.sathaye@gmail.com>
2020-12-13 16:10:25 -0800

    Create lessonPlanCode.py

---
+++ lessonPlanCode.py
@@ -0,0 +1,11 @@
+#1. Find the perimeter and area of a rectange with sides in inches: 10, 120, 10, 120
+#I want you to print the ouput as: ### inches and then #### inches squared in 
+#two separate lines.
+#Hint: To combine a integer and a string use a comma: print(##, "word")
+
+print()
+print()
+print(10+120+10+120, 'inches')
+print(10*120, 'inches squared')
+print()
+print()
